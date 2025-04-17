1cdd8d3629247b91bee646f463b84b746b112ef8
Nageshdrap <nageshachary2@gmail.com>
2025-04-17 08:52:45 +0530

    header change application json

--- src/components/confirmpage.jsx
+++ src/components/confirmpage.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Alert } from "./Alert";
 import './confirmpage.css';
 import Lottie from "lottie-react";
@@ -6,19 +6,25 @@ import loadingAnimation from "../animation/cheake.json";
 
 
 const Confirmpage = () =>{
+
+    const navigate = useNavigate();
+
+    const handleHome = () =>{
+        navigate('/');
+    }
+
     return(
         <>
                       <div className="container">
 
-                        <div style={{width:'300px',height:'250px'}}>
+                        <div className="text-center m-auto" style={{width:'300px',height:'250px'}}>
                             <Lottie animationData={loadingAnimation} loop={true} autoPlay={true} />
                         </div>
-                        <div className="text-center m-auto bg-success text-white">
-                            Your order placed successfully <button className="btn btn-outline-warning">Go to order</button>
+                        <div className="text-center m-auto bg-dark text-white px-1 py-1 d-flex justify-content-evenly align-items-center mt-4 confirmtext">
+                           <div style={{width:'70px'}}>                            <Lottie animationData={loadingAnimation} loop={true} autoPlay={true} />
+                           </div> <div className="fw-semibold fs-4">Your order placed successfully...! </div><button className="btn btn-primary w-25" onClick={handleHome}>Home</button>
                         </div>
-                        <div>
-                            <button className="btn btn-primary">Continue purchase</button>
-                        </div>
+                        
 
                       </div>
 
