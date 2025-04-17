1e7161b0a2a315299e7bc64356b56f57102787f7
Nageshdrap <nageshachary2@gmail.com>
2025-04-17 09:33:31 +0530

    header change application json

--- src/components/confirmpage.jsx
+++ src/components/confirmpage.jsx
@@ -16,15 +16,15 @@ const Confirmpage = () =>{
     return(
         <>
                       <div className="container">
-
-                        <div className="text-center m-auto" style={{width:'300px',height:'250px'}}>
+                        <div className="text-center m-auto confirmimage" style={{width:'100%',height:'500px',backgroundImage:'url("/images/confirmnew.jpg")',backgroundSize:'cover',backgroundPosition:'center'}}>
+                        <div className="text-center m-auto " style={{width:'300px',height:'250px'}}>
                             <Lottie animationData={loadingAnimation} loop={true} autoPlay={true} />
                         </div>
-                        <div className="text-center m-auto bg-dark text-white px-1 py-1 d-flex justify-content-evenly align-items-center mt-4 confirmtext">
-                           <div style={{width:'70px'}}>                            <Lottie animationData={loadingAnimation} loop={true} autoPlay={true} />
+                        <div className="text-center m-auto bg-dark text-white px-1 py-1 d-flex justify-content-evenly align-items-center mt-5 confirmtext">
+                           <div className="" style={{width:'70px'}}>                            <Lottie animationData={loadingAnimation} loop={true} autoPlay={true} />
                            </div> <div className="fw-semibold fs-4">Your order placed successfully...! </div><button className="btn btn-primary w-25" onClick={handleHome}>Home</button>
                         </div>
-                        
+                        </div>
 
                       </div>
 
