46e1b7370f98ee860e222a2f938cf201c4e13027
Nageshdrap <nageshachary2@gmail.com>
2025-04-20 13:42:38 +0530

    cart css

--- src/components/placeOrder.jsx
+++ src/components/placeOrder.jsx
@@ -236,7 +236,7 @@ export function PlaceOrder(){
         //     console.log(calculatedTotal,'nag');
         // }
         window.scrollTo({top:0,behavior:'smooth'});
-    },[])
+    },[step])
 
     return(
         <>
