16aa629f6a293e71e0ae0dc68cc0066b288805d3
Nageshdrap <nageshachary2@gmail.com>
2025-04-26 13:54:57 +0530

    offer componet update

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -5,7 +5,7 @@ const Offer = () =>{
     return(
         <>
             <div className="container-fluid  text-white py-2 text-center" style={{backgroundColor:'#4caf50'}}>
-                On first purchase you get upto 20%off apply Coupon WELCOME10
+               WEEKEND OFFER - ON FIRST PURCHASE YOU GET UPTO 20% DISCOUNT USE COUPON - WELCOME10
             </div>
         </>
     )
