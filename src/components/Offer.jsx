c8044f50989781568365e020622f409d16783258
Nageshdrap <nageshachary2@gmail.com>
2025-04-26 13:50:23 +0530

    offer componet update

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -4,7 +4,7 @@
 const Offer = () =>{
     return(
         <>
-            <div className="container-fluid  text-white " style={{backgroundColor:'#4caf50'}}>
+            <div className="container-fluid  text-white py-2 text-center" style={{backgroundColor:'#4caf50'}}>
                 On first purchase you get upto 20%off apply Coupon WELCOME10
             </div>
         </>
