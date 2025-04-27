33cb584f70bc7580753118ce9505478120cb3002
Nageshdrap <nageshachary2@gmail.com>
2025-04-27 14:26:16 +0530

    coupon component at hader

--- src/components/Coupon.jsx
+++ src/components/Coupon.jsx
@@ -5,7 +5,7 @@
 const Coupon = ({coupon}) =>{
     return(
         <>
-            <div className="position-fixed top-50 end-0 translate-middle-y bg-warning px-3 me-2" style={{display:'flex' , flexDirection:'column', alignItems:'center',zIndex:'9',color:'white',borderRadius:'11px'}}>
+            <div className="position-fixed top-50 end-0 translate-middle-y bg-warning px-3 py-2" style={{display:'flex' , flexDirection:'column', alignItems:'center',zIndex:'9',color:'white',borderRadius:'11px'}}>
                 {
                     coupon.split('').map((letter,index)=>(
                         <span key={index}>{letter}</span>
