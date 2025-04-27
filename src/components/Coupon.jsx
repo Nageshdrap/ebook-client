f4fa4598547c07aac16822286d6063c2446b73d3
Nageshdrap <nageshachary2@gmail.com>
2025-04-27 14:33:13 +0530

    coupon component at hader

--- src/components/Coupon.jsx
+++ src/components/Coupon.jsx
@@ -3,9 +3,14 @@
 
 
 const Coupon = ({coupon}) =>{
+
+    const handleCoupon = () =>{
+        window.scrollTo({top:0,behavior:'smooth'});
+    }
+
     return(
         <>
-            <div className="position-fixed top-50 end-0 translate-middle-y bg-warning px-3 py-2" style={{display:'flex' , flexDirection:'column', alignItems:'center',zIndex:'9',color:'white',borderRadius:'11px'}}>
+            <div className="position-fixed top-50 end-0 translate-middle-y bg-warning px-3 py-2" onClick={handleCoupon} style={{display:'flex' , flexDirection:'column', alignItems:'center',zIndex:'9',color:'white',borderRadius:'11px',cursor:'pointer'}}>
                 {
                     coupon.split('').map((letter,index)=>(
                         <span key={index}>{letter}</span>
