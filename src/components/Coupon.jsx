47eeae309f29d4684b5e3291f04747a600797308
Nageshdrap <nageshachary2@gmail.com>
2025-04-27 14:45:30 +0530

    produtc component width and height set for image

--- src/components/Coupon.jsx
+++ src/components/Coupon.jsx
@@ -10,7 +10,7 @@ const Coupon = ({coupon}) =>{
 
     return(
         <>
-            <div className="position-fixed top-50 end-0 translate-middle-y bg-warning px-3 py-2" onClick={handleCoupon} style={{display:'flex' , flexDirection:'column', alignItems:'center',zIndex:'9',color:'white',borderRadius:'11px',cursor:'pointer'}}>
+            <div className="position-fixed top-50 end-0 translate-middle-y bg-warning px-3 py-2" onClick={handleCoupon} style={{display:'flex' , flexDirection:'column', alignItems:'center',zIndex:'2',color:'white',borderRadius:'11px',cursor:'pointer'}}>
                 {
                     coupon.split('').map((letter,index)=>(
                         <span key={index}>{letter}</span>
