ba89e16e9aa642b02c46bb0bb42a9165ca112bed
Nageshdrap <nageshachary2@gmail.com>
2025-05-11 12:07:41 +0530

    center

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -5,8 +5,8 @@
 const Pagination = ({totalPage , page , setPage}) =>{
     return(
         <>
-            <nav aria-label="..." className="m-auto">
-                <ul class="pagination">
+            <nav aria-label="..." className="mt-3">
+                <ul class="pagination justify-content-center">
                     <li class={`page-item ${page === 1 ? 'disabled' : ''}`} style={{cursor:'pointer'}}><a  class="page-link" onClick={()=>setPage(page-1)} disabled={page === 1}>Prev</a></li>
                     {
                        [...Array(totalPage)].map((_,index)=>(
