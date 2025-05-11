ab2e0b535a8108ac0a28c008cea52ebd2dbc2a26
Nageshdrap <nageshachary2@gmail.com>
2025-05-11 11:57:40 +0530

    a tag

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -7,13 +7,13 @@ const Pagination = ({totalPage , page , setPage}) =>{
         <>
             <nav aria-label="..." className="m-auto">
                 <ul class="pagination">
-                    <li class={`page-item ${page === 1 ? 'disabled' : ''}`} ><button  class="page-link" onClick={()=>setPage(page-1)} disabled={page === 1}>Prev</button></li>
+                    <li class={`page-item ${page === 1 ? 'disabled' : ''}`} ><a  class="page-link" onClick={()=>setPage(page-1)} disabled={page === 1}>Prev</a></li>
                     {
                        [...Array(totalPage)].map((_,index)=>(
-                        <li class={`page-item ${page === index + 1 ? 'active' : ''}`}><button class="page-link" >{index + 1}</button></li>
+                        <li class={`page-item ${page === index + 1 ? 'active' : ''}`}><a class="page-link" >{index + 1}</a></li>
                        )) 
                     }
-                    <li class={`page-item ${page === totalPage ? 'disabled' : ''}`}><button class="page-link" onClick={()=>setPage(page + 1)} disabled={page === totalPage}>Next</button></li>
+                    <li class={`page-item ${page === totalPage ? 'disabled' : ''}`}><a class="page-link" onClick={()=>setPage(page + 1)} disabled={page === totalPage}>Next</a></li>
                 </ul>
             </nav>
         </>
