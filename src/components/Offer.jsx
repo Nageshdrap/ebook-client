592a65e321925d8390036b1f742675164a2f483e
Nageshdrap <nageshachary2@gmail.com>
2025-05-12 12:39:04 +0530

    shipping charge addition error at charge

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -5,7 +5,7 @@ const Offer = () =>{
     return(
         <>
             <div className="container-fluid  text-white py-2 text-center" style={{backgroundColor:'#4caf50'}}>
-               WEEKEND OFFER - ON FIRST PURCHASE YOU GET UPTO 20% DISCOUNT USE COUPON - WELCOME10
+               WEEKEND OFFER - ON FIRST PURCHASE YOU GET UPTO 20% DISCOUNT USE COUPON - WELCOME5
             </div>
         </>
     )
