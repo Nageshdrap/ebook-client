c9bd3bc203c60f1450b10b9cc9ff0e9f111dea99
Nageshdrap <nageshachary2@gmail.com>
2025-05-17 14:58:26 +0530

    login change search page

--- src/components/searchPage.jsx
+++ src/components/searchPage.jsx
@@ -3,6 +3,10 @@ import { lazy, Suspense, useEffect, useState } from "react"
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import './searchPage.css';
 import Loader from "./loader";
+import Spinner from "./spinner";
+import ProductList from "./ProductList";
+import Lottie from "lottie-react";
+import emptyAnimation from '../animation/emptyWish.json';
 // import Products from "./products";
 const Products = lazy(()=> import('./products'));
 
@@ -10,6 +14,7 @@ const Products = lazy(()=> import('./products'));
 
 export function SearchPage(){
 
+    const [loading , setLoading] = useState(false);
     const [product , setProduct] = useState([]);
     const location = useLocation();
     const query = new URLSearchParams(location.search).get("q");
@@ -17,12 +22,15 @@ export function SearchPage(){
 
     useEffect(()=>{
         const fetchSearch =async () =>{
+            setLoading(true);
            try {
             console.log(query);
             const {data} =await axios.get(`https://ebook-server-4izu.onrender.com/api/search?q=${query}`);
              setProduct(data);
            } catch (error) {
                 console.error("error in fetching");
+           }finally{
+            setLoading(false);
            }
             
         }
@@ -35,7 +43,27 @@ export function SearchPage(){
             <div className="mB">
                 <p className="text-muted">Result search for "{ query }"</p>
                 {
-                    product.length === 0 ? (<><div className=" text-center"><div className="fs-1 fw-semibold">product not found</div><div className="btn btn-primary py-2 px-3 mt-3" onClick={()=>navigate('/')} style={{width:'max-content'}}>Home</div></div></>):(<Suspense fallback={<Loader loading={true}/>}><Products product={product}/></Suspense>)
+                    loading ? (
+                         <div className="d-flex justify-content-center align-items-center mbWish" style={{ minHeight: '400px' }}>
+          <Spinner />
+        </div>
+                    ):(
+                        <>
+                            {
+                                product.length === 0 ? (
+                                     <div className="d-flex flex-column align-items-center justify-content-center" style={{ height: '500px', width: '100%' }}>
+                            <Lottie animationData={emptyAnimation} loop={true} style={{ height: '70%', width: '60%' }} />
+                            <div className="d-flex flex-column align-items-center gap-3 mt-3">
+                                <div className="fw-semibold fs-4">Product Not Found...!</div>
+                                <button className="btn btn-primary" onClick={()=>navigate('/')}>Home</button>
+                            </div>
+                        </div>
+                                ):(
+                                    <ProductList product={product}/>
+                                )
+                            }
+                        </>
+                    )
                 }
                 
             </div>
