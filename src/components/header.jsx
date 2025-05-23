bc6ccc68542e9aa9738c822a86884c8e52b07b62
Nageshdrap <nageshachary2@gmail.com>
2025-05-23 11:56:03 +0530

    wishlist beadge1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -189,7 +189,7 @@ export function Header(){
   <IoIosHeartEmpty className='fs-2' />
   {wishlist.length > 0 && (
     <span
-      className="position-absolute  translate-middle bg-danger border border-light rounded-circle"
+      className="position-absolute bg-danger p-1 border border-light rounded-circle"
       style={{ width: '2px', height: '2px' ,position:'absolute',top:'5px'}}
     ></span>
   )}
