293a85cf51db2b2cc5e98159ad7d27dcb7e56c39
Nageshdrap <nageshachary2@gmail.com>
2025-05-25 07:03:55 +0530

    cart image

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -154,9 +154,16 @@ const Cart = ({cartOpen , setCartOpen}) => {
                        return (
                            <div key={index} >
                            <div className="containern shadow gap-4 d-flex flex- justify-content-between p-2 mt-2 color-black" >
-                               <div style={{width:'130px', height:'100%', border:'1px solid grey'}}>
-                                   <img src={item.productId.images?.[0]} className="image-fluid" alt="book"  style={{width:'100%',height:'100%',cursor:'pointer',objectFit:'cover'}}/>
-                               </div>
+                                <div className="image-container">
+        <Link to={`/productDetails?pid=${item.productId._id}`}>
+        <img 
+            src={item.productId.images?.[0]} 
+            className="img-fluid" 
+            alt="book" 
+            style={{maxWidth: '100%', maxHeight: '100%'}}
+        />
+        </Link>
+    </div>
 
                                <div className="productn">
                                    <p className="mb-1 mt-0" >{item.productId.tittle}</p>
